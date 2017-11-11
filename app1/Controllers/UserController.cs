a8f51908f70f832e6e2a1dfbb2fe84286235ee14
Anderson Koester <anderson@koester.com.br>
2017-11-11 12:42:10 -0200

    studing c# .net rest api

---
+++ app1/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using app1.Models;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app1.Controllers {
+
+    [Route("api/user")]
+    public class UserController : Controller {
+        
+        private static List<UserModel> userList = new List<UserModel>();
+
+        // GET: api/user
+        [HttpGet]
+        public IActionResult getUserList(HttpRequestMessage request) {
+            return new ObjectResult(userList);
+        }
+
+        // GET api/user/5
+        [HttpGet("{code}")]
+        public UserModel getUser(int code) {
+            UserModel user = userList.Where(n => n.Code == code)
+                .Select(n => n)
+                .First();
+            return user;
+        }
+
+        // POST api/user
+        [HttpPost]
+        public UserModel createUser([FromBody] UserModel user) {
+            userList.Add(user);
+            return user;
+        }
+
+        // PUT api/user/5
+        [HttpPut("{code}")]
+        public UserModel updateUser(int code, [FromBody] UserModel user) {
+            UserModel userResult = new UserModel();
+            userList.Where(n => n.Code == code)
+                .Select(s => {
+                    s.Code = user.Code;
+                    s.Login = user.Login;
+                    s.Name = user.Name;
+                    userResult = s;
+                    return s;
+                }).ToList();
+
+            return userResult;
+        }
+
+        // DELETE api/user/5
+        [HttpDelete("{code}")]
+        public string deleteUser(int code) {
+            UserModel user = userList.Where(n => n.Code == code)
+                .Select(n => n)
+                .First();
+
+            userList.Remove(user);
+
+            return "Registro excluido";
+        }
+    }
+}
---
+++ app1/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app1.Models{
+    public class UserModel{
+        private int code;
+        private string name;
+        private string login;
+
+        public UserModel(){}
+
+        public UserModel(int code, string name, string login) {
+            this.Code = code;
+            this.Name = name;
+            this.Login = login;
+        }
+
+        public int Code { get => code; set => code = value; }
+        public string Name { get => name; set => name = value; }
+        public string Login { get => login; set => login = value; }
+    }
+}
